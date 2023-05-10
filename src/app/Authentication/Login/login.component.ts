36b288791024e8e6b157ce70f2c9d60f487c1591
rahul.rv <rahul.rv@inapp.com>
2023-05-10 14:47:26 +0530

    Added authentication module

---
+++ src/app/Authentication/Login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component,OnInit  } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  form: FormGroup = new FormGroup({});
+  flag: boolean = true;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: [null, [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  saveDetails(form: any) {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(form.value, null, 4));
+  }
+    
+}
---
+++ src/app/Authentication/authentication.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './Login/login.component';
+import { CommonModule } from '@angular/common';
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+@NgModule({
+  declarations: [
+    LoginComponent,
+  ],
+  imports: [
+      CommonModule,
+      AuthenticationRoutingModule,
+      FormsModule,
+      MatSlideToggleModule,
+      MatButtonModule,
+      ReactiveFormsModule,
+      MatNativeDateModule,
+      MatDatepickerModule,
+      MatInputModule,
+      MatSortModule,
+      MatTableModule,
+      MatIconModule,
+      MatSelectModule,
+      MatCardModule,
+      MatRadioModule,
+      
+  ],
+  providers: [],
+})
+export class AuthenticationModule { }
